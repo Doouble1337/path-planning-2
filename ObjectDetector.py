ea99e2db2586a8622dd310c125f602bd6010ba20
Fedor Dakhnenko <fedorrrd@gmail.com>
2021-05-28 11:55:55 +0300

    object detector commit

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -3,7 +3,6 @@ import cv2
 import time
 from IPython.display import clear_output
 
-
 def get_available_cameras(upper_bound=10, lower_bound=0):
     available = []
 
@@ -17,35 +16,32 @@ def get_available_cameras(upper_bound=10, lower_bound=0):
 
     return available
 
-
-get_available_cameras()
-
-cam1 = cv2.VideoCapture(0)
-
-cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-cv2.namedWindow("sliders_frame", cv2.WINDOW_NORMAL)
-
-cv2.resizeWindow("frame", (960, 720))
-
-lower_bound = np.array([160, 100, 40])
-upper_bound = np.array([190, 130, 60])
-
-
 def on_change(x):
     print(lower_bound, upper_bound)
 
+#while true:
 
-colors = []
+def detect_objects():
+    get_available_cameras()
 
-cv2.createTrackbar('lower_red', 'sliders_frame', 0, 255, on_change)
-cv2.createTrackbar('lower_green', 'sliders_frame', 115, 255, on_change)
-cv2.createTrackbar('lower_blue', 'sliders_frame', 0, 255, on_change)
+    cam1 = cv2.VideoCapture(0)
 
-cv2.createTrackbar('upper_red', 'sliders_frame', 30, 255, on_change)
-cv2.createTrackbar('upper_green', 'sliders_frame', 255, 255, on_change)
-cv2.createTrackbar('upper_blue', 'sliders_frame', 255, 255, on_change)
+    cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("sliders_frame", cv2.WINDOW_NORMAL)
 
-while (True):
+    cv2.resizeWindow("frame", (960, 720))
+
+    lower_bound = np.array([160, 100, 40])
+    upper_bound = np.array([190, 130, 60])
+
+    cv2.createTrackbar('lower_red', 'sliders_frame', 0, 255, on_change)
+    cv2.createTrackbar('lower_green', 'sliders_frame', 115, 255, on_change)
+    cv2.createTrackbar('lower_blue', 'sliders_frame', 0, 255, on_change)
+
+    cv2.createTrackbar('upper_red', 'sliders_frame', 30, 255, on_change)
+    cv2.createTrackbar('upper_green', 'sliders_frame', 255, 255, on_change)
+    cv2.createTrackbar('upper_blue', 'sliders_frame', 255, 255, on_change)
+
     ret1, frame = cam1.read()
 
     cv2.waitKey(1)
@@ -69,12 +65,13 @@ while (True):
         if stat[4] > 1000:
             cv2.rectangle(frame, (stat[0], stat[1]), (stat[0] + stat[2], stat[1] + stat[3]), (0, 255, 255), 1)
 
-    cv2.imshow("frame", frame)
-    cv2.imshow("mask", mask)
+    #cv2.imshow("frame", frame)
+    #cv2.imshow("mask", mask)
 
-    if (cv2.waitKey(1) & 0xFF == ord('q')):
-        break
+    #if (cv2.waitKey(1) & 0xFF == ord('q')):
+     #   break
 
-cam1.release()
+    #cam1.release()
 
-cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
+    return (mask)

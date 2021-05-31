cdae0e3ea3d87b79f502966c6cc333fe36928ce7
Mashamell0w <masha.koldubaeva@gmail.com>
2021-05-31 20:33:36 +0300

    scale listbox button commit

--- GUI.py
+++ GUI.py
@@ -1,14 +1,46 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
+root = Tk()
 
-frame1 = tk.Frame(master=window, width=800, height=100, bg="grey")
-frame1.pack(fill=tk.BOTH, expand=True)
+root.geometry('1100x700')
 
-frame2 = tk.Frame(master=window, width=800, height=500, bg="DarkGrey")
-frame2.pack(fill=tk.BOTH, expand=True)
+det = Scale(root, orient=HORIZONTAL, length=250)
+det.grid(row=0, column=6, columnspan=4)
 
-frame3 = tk.Frame(master=window, width=800, height=100, bg="LightGrey")
-frame3.pack(fill=tk.BOTH, expand=True)
+scale1 = Scale(root, orient=HORIZONTAL, length=250)
+scale1.grid(row=0, column=0, columnspan=4)
 
-window.mainloop()
+scale2 = Scale(root, orient=HORIZONTAL, length=250)
+scale2.grid(row=1, column=0, columnspan=4)
+
+scale3 = Scale(root, orient=HORIZONTAL, length=250)
+scale3.grid(row=2, column=0, columnspan=4)
+
+scale4 = Scale(root, orient=HORIZONTAL, length=250)
+scale4.grid(row=3, column=0, columnspan=4)
+
+scale5 = Scale(root, orient=HORIZONTAL, length=250)
+scale5.grid(row=4, column=0, columnspan=4)
+
+scale6 = Scale(root, orient=HORIZONTAL, length=250)
+scale6.grid(row=5, column=0, columnspan=4)
+
+but_sub = Button(root, text='Submit')
+but_sub.grid(row=25, column=30)
+
+Lb = Listbox(root, height=3)
+Lb.grid(row=7, column=0, columnspan=4)
+Lb.insert(1, "1")
+Lb.insert(2, "2")
+Lb.insert(3, "3")
+Lb.insert(4, "4")
+Lb.insert(5, "5")
+Lb.insert(6, "6")
+
+col_count, row_count = root.grid_size()
+for col in range(col_count):
+    root.grid_columnconfigure(col, minsize=20)
+for row in range(row_count):
+    root.grid_rowconfigure(row, minsize=20)
+
+root.mainloop()

1d99ac35e34b9872b922e38f261f58bce885af55
Mashamell0w <masha.koldubaeva@gmail.com>
2021-06-02 11:07:48 +0300

    Image resize commit

--- GUI.py
+++ GUI.py
@@ -7,10 +7,12 @@ root.geometry('1170x700')
 
 scale_list = [0, 0, 0, 0, 0, 0, 0]
 
-det = Scale(root, orient=HORIZONTAL, length=250, label="determine", to=255, command=lambda i: func_det(i))
+a = 100
+
+det = Scale(root, orient=HORIZONTAL, length=250, label="determine", to=a, command=lambda i: func_det(i))
 det.grid(row=0, column=6, columnspan=4)
 
-scale1 = Scale(root, orient=HORIZONTAL, length=250, label="lower_red", to=255, command=lambda i: func1(i))
+scale1 = Scale(root, orient=HORIZONTAL, length=250, label="lower_red", to=a, command=lambda i: func1(i))
 scale1.grid(row=0, column=0, columnspan=4)
 
 scale2 = Scale(root, orient=HORIZONTAL, length=250, label="lower_green", to=255, command=lambda i: func2(i))
@@ -80,32 +82,16 @@ def func6(i):
 
 canvas1 = Canvas(root, height=400, width=400)
 image1 = Image.open("D:\Image_test\Test_image.jpg")
-photo1 = ImageTk.PhotoImage(image1)
+resized1 = image1.resize((400, 400))
+photo1 = ImageTk.PhotoImage(resized1)
 image1 = canvas1.create_image(0, 0, anchor='nw', image=photo1)
 canvas1.grid(row=1, column=4, columnspan=15, rowspan=15)
 
 canvas2 = Canvas(root, height=400, width=400)
 image2 = Image.open("D:\Image_test\Test_image2.jpg")
-photo2 = ImageTk.PhotoImage(image2)
+resized2 = image2.resize((400, 400))
+photo2 = ImageTk.PhotoImage(resized2)
 image2 = canvas2.create_image(0, 0, anchor='nw', image=photo2)
-canvas2.grid(row=1, column=18, columnspan=15, rowspan=15)
+canvas2.grid(row=1, column=19, columnspan=6, rowspan=15)
 
 root.mainloop()
-
-# canvas1 = Canvas(root, height=400, width=400)
-# image1 = Image.open("D:\Image_test\Test_image.jpg")
-# resized1 = image1.resize((800, 600), Image.ANTIALIAS)
-# photo1 = ImageTk.PhotoImage(resized1)
-# image1 = canvas1.create_image(0, 0, anchor='nw', image=resized1)
-
-# original = Image.open('D:\Image_test\Test_image.jpg')
-# image = ImageTk.PhotoImage(original)
-# display = Canvas(root)
-# display.create_image(0, 0, image=image, tags="IMG")
-# display.grid(row=2, column=4, columnspan=15, rowspan=15)
-
-# size = (canvas1.size, canvas1.size)
-# resized = original.resize(size, Image.ANTIALIAS)
-# image = ImageTk.PhotoImage(resized)
-# display.delete("IMG")
-# display.create_image(0, 0, image=image, tags="IMG")

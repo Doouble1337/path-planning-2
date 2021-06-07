e29c6f26095c42bf40b9b328bb24585e974f2ee2
Doouble <katulevskiy@gmail.com>
2021-06-07 11:57:54 +0300

    LIDAR v4 -  FULL WORKING LIDAR DATA ANALYSIS

--- for test.py
+++ for test.py
@@ -1,16 +1,4 @@
-from sklearn.linear_model import LinearRegression
-
-data = [[1, 2], [3, 4]]
-print(data)
-print(data[0])
-print(data[0][1])
-
-data1 = [[[1, 2], [3,4]], [[5, 6], [7, 8]]]
-print(data1[0][1][1])
-
-line = [[[]]]
-line[0][0].append(1)
-print(line)
-lines = [[[1, 1], [2, 2]], [[100, 100]], [[3, 3], [4, 4]]]
-lines[-1].append(lines[0])
-print(*lines)
+import numpy as np
+xs=[[1], [2], [0], [-1]]
+s = np.sort(xs, axis = 0)
+print(s)

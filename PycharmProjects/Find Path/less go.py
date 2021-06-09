4f9709996b01fa24b5a4a3b8ba84fcc4d1be4de7
l3cire <mr.vadimzaripov@gmail.com>
2021-06-09 23:30:51 +0300

    clean commit

--- PycharmProjects/Find Path/less go.py
+++
@@ -1,9 +0,0 @@
-import dijkstra
-field_sz = 1000 #не забыть поменять
-cell_sz = 10 #не забыть поменять
-angle = 90
-directions = 360 // angle
-start = [0, 0]
-finish = [field_sz, field_sz]
-
-#сегенерировать кратчайшую траекторию от start к finish
--- for test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-xs=[[1], [2], [0], [-1]]
-s = np.sort(xs, axis = 0)
-print(s)

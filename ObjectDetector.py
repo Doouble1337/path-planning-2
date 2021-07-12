d5903cd3f2eff6e5251395d7d89625015cf6476e
Fedor Dakhnenko <fedorrrd@gmail.com>
2021-07-12 11:14:52 +0300

    object detector commit full version

--- ObjectDetector.py
+++ ObjectDetector.py
@@ -15,18 +15,14 @@ def get_available_cameras(upper_bound=10, lower_bound=0):
 
     return available
 
-def on_change(x):
-    print(lower_bound, upper_bound)
-
 class ObjectDetector:
 
     @staticmethod
     def detect_object(image, bounds, approximation_coefficient, morphology_coefficient):
-        morph = cv2.morphologyEx(image, cv2.MORPH_OPEN, np.ones((morphology_coefficient, morphology_coefficient), np.uint8), iterations=1)
-
-
-        #print(approximation_coefficient)
         lower, upper = bounds
+        morph = cv2.morphologyEx(image, cv2.MORPH_OPEN,
+                                 np.ones((morphology_coefficient, morphology_coefficient), np.uint8), iterations=1)
+
         table_mask = cv2.inRange(morph, lower, upper)
 
         connect = cv2.connectedComponentsWithStats(table_mask, 4, cv2.CV_32S)
@@ -45,21 +41,15 @@ class ObjectDetector:
                 if stat[1]+stat[3] > down_side:
                     down_side=stat[1]+stat[3]
 
-
         ret, mask = cv2.threshold(morph[upper_side:down_side ,left_side:right_side, 0], 0, 255, cv2.THRESH_OTSU)
 
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         hull = contours
-        #for i in range(len(contours)):
-        #    hull.append(cv2.convexHull(contours[i], False))
-
 
         for i in range(len(hull)):
             epsilon = approximation_coefficient * cv2.arcLength(hull[i], True)
             hull[i] = cv2.approxPolyDP(hull[i], epsilon, True)
-
-
 
         contours_image = np.zeros_like(image)
         cv2.drawContours(contours_image, contours, -1, (255, 255, 255), 1)
